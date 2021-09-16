6d1b244f16f20606390bc082bc4c66d5cb2b8b96
dragan1810 <d.novakovic1810@yahoo.com>
2021-09-16 20:11:09 +0200

    lol

--- ham-end/pallets/ham/src/lib.rs
+++ ham-end/pallets/ham/src/lib.rs
@@ -12,7 +12,27 @@ pub mod pallet_ham {
 		traits::{Currency, ExistenceRequirement, Randomness},
 	};
 	use frame_system::pallet_prelude::*;
-	// use sp_core::H256;
+	use sp_core::H256;
+
+	impl Default for HamKind {
+		fn default() -> Self {
+			HamKind::Regular
+		}
+	}
+
+	#[derive(Clone, Encode, Decode, PartialEq)]
+	pub enum HamKind {
+		PataNegra,
+		Regular,
+	}
+
+	#[derive(Clone, Encode, Decode, Default, PartialEq)]
+	pub struct Ham<Hash, Balance> {
+		id: Hash,
+		origin: Hash,
+		price: Balance,
+		ham_type: HamKind,
+	}
 
 	#[pallet::pallet]
 	#[pallet::generate_store(trait Store)]
@@ -23,6 +43,7 @@ pub mod pallet_ham {
 	pub trait Config: pallet_balances::Config + frame_system::Config {
 		/// Because this pallet emits events, it dependes on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type HamRandomness: Randomness<H256, u32>;
 	}
 
 	#[pallet::error]
@@ -34,8 +55,17 @@ pub mod pallet_ham {
 	pub enum Event<T: Config> {}
 
 	#[pallet::storage]
+	#[pallet::getter(fn hams)]
+	pub(super) type Hams<T: Config> =
+		StorageMap<_, Twox64Concat, T::Hash, Ham<T::Hash, T::Balance>, ValueQuery>;
+
+	#[pallet::storage]
 	#[pallet::getter(fn all_hams_count)]
 	pub(super) type AllHamsCount<T: Config> = StorageValue<_, u64, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn get_nonce)]
+	pub(super) type Nonce<T: Config> = StorageValue<_, u64, ValueQuery>;
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
@@ -43,6 +73,20 @@ pub mod pallet_ham {
 	}
 
 	impl<T: Config> Pallet<T> {
-		// helper stuff
+		fn increment_nonce() -> DispatchResult {
+			<Nonce<T>>::try_mutate(|nonce| {
+				let next = nonce.checked_add(1).ok_or("Overflow")?;
+				*nonce = next;
+
+				Ok(().into())
+			})
+		}
+
+		fn random_hash(sender: &T::AccountId) -> T::Hash {
+			let nonce = <Nonce<T>>::get();
+			let seed = T::HamRandomness::random_seed();
+
+			T::Hashing::hash_of(&(seed, &sender, nonce))
+		}
 	}
 }
--- ham-end/runtime/src/lib.rs
+++ ham-end/runtime/src/lib.rs
@@ -271,6 +271,7 @@ impl pallet_sudo::Config for Runtime {
 /// Configure the pallet-template in pallets/template.
 impl pallet_template::Config for Runtime {
 	type Event = Event;
+	type HamRandomness = RandomnessCollectiveFlip;
 }
 
 // Create the runtime by composing the FRAME pallets that were previously configured.
