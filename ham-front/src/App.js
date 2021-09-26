225999b827071b39eb3d803fa60f90d0a400f82d
dragan1810 <d.novakovic1810@yahoo.com>
2021-09-26 21:41:29 +0200

    qqq

--- ham-front/src/App.js
+++ ham-front/src/App.js
@@ -1,49 +1,63 @@
-import React, { useState, createRef } from 'react';
-import { Container, Dimmer, Loader, Grid, Sticky, Message } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
+import React, { useState, createRef } from "react";
+import {
+  Container,
+  Dimmer,
+  Loader,
+  Grid,
+  Sticky,
+  Message,
+} from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
-import { SubstrateContextProvider, useSubstrate } from './substrate-lib';
-import { DeveloperConsole } from './substrate-lib/components';
+import { SubstrateContextProvider, useSubstrate } from "./substrate-lib";
+import { DeveloperConsole } from "./substrate-lib/components";
 
-import AccountSelector from './AccountSelector';
-import Balances from './Balances';
-import BlockNumber from './BlockNumber';
-import Events from './Events';
-import Interactor from './Interactor';
-import Metadata from './Metadata';
-import NodeInfo from './NodeInfo';
-import TemplateModule from './TemplateModule';
-import Transfer from './Transfer';
-import Upgrade from './Upgrade';
+import AccountSelector from "./AccountSelector";
+import Balances from "./Balances";
+import BlockNumber from "./BlockNumber";
+import Events from "./Events";
+import Interactor from "./Interactor";
+import Metadata from "./Metadata";
+import NodeInfo from "./NodeInfo";
+import TemplateModule from "./TemplateModule";
+import Transfer from "./Transfer";
+import Upgrade from "./Upgrade";
 
-function Main () {
+function Main() {
   const [accountAddress, setAccountAddress] = useState(null);
   const { apiState, keyring, keyringState, apiError } = useSubstrate();
   const accountPair =
     accountAddress &&
-    keyringState === 'READY' &&
+    keyringState === "READY" &&
     keyring.getPair(accountAddress);
 
-  const loader = text =>
+  const loader = (text) => (
     <Dimmer active>
-      <Loader size='small'>{text}</Loader>
-    </Dimmer>;
+      <Loader size="small">{text}</Loader>
+    </Dimmer>
+  );
 
-  const message = err =>
+  const message = (err) => (
     <Grid centered columns={2} padded>
       <Grid.Column>
-        <Message negative compact floating
-          header='Error Connecting to Substrate'
+        <Message
+          negative
+          compact
+          floating
+          header="Error Connecting to Substrate"
           content={`${JSON.stringify(err, null, 4)}`}
         />
       </Grid.Column>
-    </Grid>;
+    </Grid>
+  );
 
-  if (apiState === 'ERROR') return message(apiError);
-  else if (apiState !== 'READY') return loader('Connecting to Substrate');
+  if (apiState === "ERROR") return message(apiError);
+  else if (apiState !== "READY") return loader("Connecting to Substrate");
 
-  if (keyringState !== 'READY') {
-    return loader('Loading accounts (please review any extension\'s authorization)');
+  if (keyringState !== "READY") {
+    return loader(
+      "Loading accounts (please review any extension's authorization)"
+    );
   }
 
   const contextRef = createRef();
@@ -54,7 +68,7 @@ function Main () {
         <AccountSelector setAccountAddress={setAccountAddress} />
       </Sticky>
       <Container>
-        <Grid stackable columns='equal'>
+        <Grid stackable columns="equal">
           <Grid.Row stretched>
             <NodeInfo />
             <Metadata />
@@ -64,16 +78,13 @@ function Main () {
           <Grid.Row stretched>
             <Balances />
           </Grid.Row>
-          <Grid.Row>
-            <Transfer accountPair={accountPair} />
-            <Upgrade accountPair={accountPair} />
-          </Grid.Row>
+
           <Grid.Row>
             <Interactor accountPair={accountPair} />
             <Events />
           </Grid.Row>
           <Grid.Row>
-            <TemplateModule accountPair={accountPair} />
+            <Transfer accountPair={accountPair} />
           </Grid.Row>
         </Grid>
       </Container>
@@ -82,7 +93,7 @@ function Main () {
   );
 }
 
-export default function App () {
+export default function App() {
   return (
     <SubstrateContextProvider>
       <Main />
--- ham-front/src/Balances.js
+++ ham-front/src/Balances.js
@@ -1,26 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { Table, Grid, Button } from 'semantic-ui-react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { useSubstrate } from './substrate-lib';
+import React, { useEffect, useState } from "react";
+import { Table, Grid, Button } from "semantic-ui-react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useSubstrate } from "./substrate-lib";
 
-export default function Main (props) {
+export default function Main(props) {
   const { api, keyring } = useSubstrate();
   const accounts = keyring.getPairs();
   const [balances, setBalances] = useState({});
 
   useEffect(() => {
-    const addresses = keyring.getPairs().map(account => account.address);
+    const addresses = keyring.getPairs().map((account) => account.address);
     let unsubscribeAll = null;
 
     api.query.system.account
-      .multi(addresses, balances => {
-        const balancesMap = addresses.reduce((acc, address, index) => ({
-          ...acc, [address]: balances[index].data.free.toHuman()
-        }), {});
+      .multi(addresses, (balances) => {
+        const balancesMap = addresses.reduce(
+          (acc, address, index) => ({
+            ...acc,
+            [address]: balances[index].data.free.toHuman(),
+          }),
+          {}
+        );
         setBalances(balancesMap);
-      }).then(unsub => {
+      })
+      .then((unsub) => {
         unsubscribeAll = unsub;
-      }).catch(console.error);
+      })
+      .catch(console.error);
 
     return () => unsubscribeAll && unsubscribeAll();
   }, [api, keyring, setBalances]);
@@ -28,10 +34,10 @@ export default function Main (props) {
   return (
     <Grid.Column>
       <h1>Balances</h1>
-      <Table celled striped size='small'>
+      <Table celled striped size="small">
         <Table.Body>
           <Table.Row>
-            <Table.Cell width={3} textAlign='right'>
+            <Table.Cell width={3} textAlign="right">
               <strong>Name</strong>
             </Table.Cell>
             <Table.Cell width={10}>
@@ -41,11 +47,13 @@ export default function Main (props) {
               <strong>Balance</strong>
             </Table.Cell>
           </Table.Row>
-          {accounts.map(account =>
+          {accounts.slice(0, 3).map((account) => (
             <Table.Row key={account.address}>
-              <Table.Cell width={3} textAlign='right'>{account.meta.name}</Table.Cell>
+              <Table.Cell width={3} textAlign="right">
+                {account.meta.name}
+              </Table.Cell>
               <Table.Cell width={10}>
-                <span style={{ display: 'inline-block', minWidth: '31em' }}>
+                <span style={{ display: "inline-block", minWidth: "31em" }}>
                   {account.address}
                 </span>
                 <CopyToClipboard text={account.address}>
@@ -53,18 +61,19 @@ export default function Main (props) {
                     basic
                     circular
                     compact
-                    size='mini'
-                    color='blue'
-                    icon='copy outline'
+                    size="mini"
+                    color="blue"
+                    icon="copy outline"
                   />
                 </CopyToClipboard>
               </Table.Cell>
-              <Table.Cell width={3}>{
-                balances && balances[account.address] &&
-                balances[account.address]
-              }</Table.Cell>
+              <Table.Cell width={3}>
+                {balances &&
+                  balances[account.address] &&
+                  balances[account.address]}
+              </Table.Cell>
             </Table.Row>
-          )}
+          ))}
         </Table.Body>
       </Table>
     </Grid.Column>
