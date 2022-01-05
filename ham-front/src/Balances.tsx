28b08055db230ae16ff2a6adaadcf8b19498d72b
dragan1810 <d.novakovic1810@yahoo.com>
2022-01-05 13:04:57 +0100

    qqqq

--- ham-front/src/Balances.tsx
+++ ham-front/src/Balances.tsx
@@ -3,19 +3,19 @@ import { Table, Grid, Button } from "semantic-ui-react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { useSubstrate } from "./substrate-lib";
 
-export default function Main(props) {
+export default function Main(props: any) {
   const { api, keyring } = useSubstrate();
   const accounts = keyring.getPairs();
   const [balances, setBalances] = useState({});
 
   useEffect(() => {
-    const addresses = keyring.getPairs().map((account) => account.address);
-    let unsubscribeAll = null;
+    const addresses = keyring.getPairs().map((account: any) => account.address);
+    let unsubscribeAll: any = null;
 
     api.query.system.account
-      .multi(addresses, (balances) => {
+      .multi(addresses, (balances: any) => {
         const balancesMap = addresses.reduce(
-          (acc, address, index) => ({
+          (acc: any, address: any, index: any) => ({
             ...acc,
             [address]: balances[index].data.free.toHuman(),
           }),
@@ -23,12 +23,12 @@ export default function Main(props) {
         );
         setBalances(balancesMap);
       })
-      .then((unsub) => {
+      .then((unsub: any) => {
         unsubscribeAll = unsub;
       })
       .catch(console.error);
 
-    return () => unsubscribeAll && unsubscribeAll();
+    return () => unsubscribeAll?.();
   }, [api, keyring, setBalances]);
 
   return (
@@ -47,7 +47,7 @@ export default function Main(props) {
               <strong>Balance</strong>
             </Table.Cell>
           </Table.Row>
-          {accounts.slice(0, 3).map((account) => (
+          {accounts.slice(0, 3).map((account: any) => (
             <Table.Row key={account.address}>
               <Table.Cell width={3} textAlign="right">
                 {account.meta.name}
@@ -68,9 +68,9 @@ export default function Main(props) {
                 </CopyToClipboard>
               </Table.Cell>
               <Table.Cell width={3}>
-                {balances &&
-                  balances[account.address] &&
-                  balances[account.address]}
+                {/* 
+                  ///@ts-ignore      */}
+                {balances && balances?.[account.address]}
               </Table.Cell>
             </Table.Row>
           ))}
