29885616c60715c05108d2c5fc2d49b621ce47cf
dragan1810 <d.novakovic1810@yahoo.com>
2022-01-12 22:33:35 +0100

    qqq

--- ham-front/src/HamOps/CreateHam.tsx
+++ ham-front/src/HamOps/CreateHam.tsx
@@ -3,41 +3,51 @@ import { SubmittableModuleExtrinsics } from "@polkadot/api/types";
 import React, { useRef, useEffect, useState } from "react";
 import { Button, Form } from "semantic-ui-react";
 import { useSubstrate } from "../substrate-lib";
+import { isOptional } from "../utils";
+
+// To-Do!
+//  const metaArgs = _api.tx["hamModule"]["createHam"].meta.args;
+//   const paramFields = metaArgs.map((arg: any) => ({
+//     name: arg.name.toString(),
+//     type: arg.type.toString(),
+//     optional: isOptional(arg.type.toString()),
+//   }));
 
 export function CreateHam() {
   const { api } = useSubstrate();
-  const [, setPalletRPCs] = useState<string[]>([]);
-  const [txModule, setTxModule] =
-    useState<SubmittableModuleExtrinsics<"promise">>();
+  const [_ham_count, setHamCount] = useState<any>();
   const _api: ApiPromise = api;
   const hamKindRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    const palletRPCs = Object.keys(api.tx);
-    setPalletRPCs(palletRPCs);
-    setTxModule(_api.tx["hamModule"]);
+    _api.query["hamModule"].allHamsCount().then((r) => {
+      setHamCount(r.toString());
+      // setHamCount(ham_count);
+    });
   }, [api]);
 
   const sumbit = async () => {
-    const metaArgs = _api.tx["hamModule"]["createHam"].meta.args;
-    const paramFields = metaArgs.map((arg: any) => ({
-      name: arg.name.toString(),
-      type: arg.type.toString(),
-      optional: true,
-    }));
-
-    console.log({ paramFields });
-    // console.log(_api.query["hamModule"].hams());
+    _api.tx["hamModule"]
+      .createHam(null)
+      .signAndSend(fromAcct, txResHandler)
+      .catch(txErrHandler);
   };
   return (
-    <Form>
-      <Form.Field>
-        <label>HamKind</label>
-        <input ref={hamKindRef} placeholder="HamKind" />
-      </Form.Field>
-      <Button type="submit" onClick={sumbit}>
-        Submit
-      </Button>
-    </Form>
+    <div style={{ border: "1px solid black", padding: 100 }}>
+      <div>Current ham count: {_ham_count} </div>
+      <Form>
+        <Form.Field>
+          <label>Ham Type</label>
+          <input
+            disabled={true}
+            ref={hamKindRef}
+            placeholder="Ham Type (Optional)"
+          />
+        </Form.Field>
+        <Button type="submit" onClick={sumbit}>
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
 }
--- ham-front/src/utils/index.ts
+++ ham-front/src/utils/index.ts
@@ -1,8 +1,14 @@
 // Construct a Ham ID from storage key
-const convertToHamHash = (entry) => `0x${entry[0].toJSON().slice(-64)}`;
+
+import { useState } from "react";
+import { useSubstrate } from "../substrate-lib";
+
+export const isOptional = (type: string) => type.startsWith("Option<");
+
+const convertToHamHash = (entry: any) => `0x${entry[0].toJSON().slice(-64)}`;
 
 // Construct a Ham object
-const constructHam = (hash, { dna, price, gender, owner }) => ({
+const constructHam = (hash: any, { dna, price, gender, owner }: any) => ({
   id: hash,
   dna,
   price: price.toJSON(),
@@ -11,7 +17,7 @@ const constructHam = (hash, { dna, price, gender, owner }) => ({
 });
 
 // Use React hooks
-export default function Hams(props) {
+export default function Hams(props: any) {
   const { api, keyring } = useSubstrate();
   const { accountPair } = props;
 
