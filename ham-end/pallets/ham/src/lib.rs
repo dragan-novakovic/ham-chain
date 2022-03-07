99702a18acdcaeb9e2af175168a8fdd7a5c4c6d5
dragan1810 <d.novakovic1810@yahoo.com>
2022-03-07 21:30:23 +0100

    qqq

--- ham-end/pallets/ham/src/lib.rs
+++ ham-end/pallets/ham/src/lib.rs
@@ -101,13 +101,16 @@ pub mod pallet_ham {
 		NonceOverflow,
 		HamAddOverflow,
 		BuyerIsHamOwner,
+		BuyerisAnimalOwner,
 		/// Cannot transfer a ham to its owner.
 		TransferToSelf,
 		/// Handles checking whether the Ham exists.
 		HamNotExist,
+		AnimalNotExist,
 		/// Handles checking that the Ham is owned by the account transferring, buying or setting a
 		/// price for it.
 		NotHamOwner,
+		NotAnimalOwner,
 		/// Ensures the Ham is for sale.
 		HamNotForSale,
 		/// Ensures that the buying price is greater than the asking price.
@@ -260,7 +263,7 @@ pub mod pallet_ham {
 			let from = ensure_signed(origin)?;
 
 			// Ensure the ham exists and is called by the ham owner
-			ensure!(Self::is_animal_owner(&animal_id, &from)?, <Error<T>>::NotHamOwner);
+			ensure!(Self::is_animal_owner(&animal_id, &from)?, <Error<T>>::NotAnimalOwner);
 
 			// Verify the ham is not transferring back to its owner.
 			ensure!(from != to, <Error<T>>::TransferToSelf);
@@ -413,9 +416,10 @@ pub mod pallet_ham {
 			animal_id: &[u8; 16],
 			acct: &T::AccountId,
 		) -> Result<bool, Error<T>> {
+			// log?
 			match Self::animals(animal_id) {
 				Some(animal) => Ok(animal.owner == *acct),
-				None => Err(<Error<T>>::HamNotExist),
+				None => Err(<Error<T>>::AnimalNotExist),
 			}
 		}
 
@@ -435,73 +439,3 @@ pub mod pallet_ham {
 		}
 	}
 }
-
-// #[cfg(test)]
-// mod mock;
-
-// #[cfg(test)]
-// mod tests;
-
-// #[cfg(feature = "runtime-benchmarks")]
-// mod benchmarking;
-
-// 	#[pallet::storage]
-// 	#[pallet::getter(fn something)]
-// 	// Learn more about declaring storage items:
-// 	// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-// 	pub type Something<T> = StorageValue<_, u32>;
-
-// 	// Pallets use events to inform users when important changes are made.
-// 	// https://substrate.dev/docs/en/knowledgebase/runtime/events
-// 	#[pallet::event]
-// 	#[pallet::metadata(T::AccountId = "AccountId")]
-// 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-// 	pub enum Event<T: Config> {
-// 		/// Event documentation should end with an array that provides descriptive names for event
-// 		/// parameters. [something, who]
-// 		SomethingStored(u32, T::AccountId),
-// 	}
-
-// 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-// 	// These functions materialize as "extrinsics", which are often compared to transactions.
-// 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
-// 	#[pallet::call]
-// 	impl<T: Config> Pallet<T> {
-// 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-// 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-// 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-// 		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-// 			// Check that the extrinsic was signed and get the signer.
-// 			// This function will return an error if the extrinsic is not signed.
-// 			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
-// 			let who = ensure_signed(origin)?;
-
-// 			// Update storage.
-// 			<Something<T>>::put(something);
-
-// 			// Emit an event.
-// 			Self::deposit_event(Event::SomethingStored(something, who));
-// 			// Return a successful DispatchResultWithPostInfo
-// 			Ok(())
-// 		}
-
-// 		/// An example dispatchable that may throw a custom error.
-// 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-// 		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-// 			let _who = ensure_signed(origin)?;
-
-// 			// Read a value from storage.
-// 			match <Something<T>>::get() {
-// 				// Return an error if the value has not been set.
-// 				None => Err(Error::<T>::NoneValue)?,
-// 				Some(old) => {
-// 					// Increment the value read from storage; will error in the event of overflow.
-// 					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-// 					// Update the value in storage with the incremented result.
-// 					<Something<T>>::put(new);
-// 					Ok(())
-// 				}
-// 			}
-// 		}
-// 	}
-// }
